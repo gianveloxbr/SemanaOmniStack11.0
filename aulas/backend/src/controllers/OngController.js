1b8a7b7cc70b14ba8ef05f1772789daf74049dcb
Gian Michel <gianmnf@unipam.edu.br>
2020-03-27 11:26:41 -0300

    Aula 5 - 27/03

--- aulas/backend/src/controllers/OngController.js
+++ aulas/backend/src/controllers/OngController.js
@@ -1,5 +1,5 @@
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
-const crypto = require('crypto');
 
 module.exports = {
 
@@ -12,7 +12,7 @@ module.exports = {
     async create(request, response) {
         const { name, email, whatsapp, city, uf} = request.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
@@ -22,6 +22,7 @@ module.exports = {
             city,
             uf
         })
+        
         return response.json({ id });
     }
 }
